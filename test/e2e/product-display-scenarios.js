f8b5444383ad486baf659b08f370921de7de678b
tjchaplin <tjchaplin@hotmail.com>
2014-06-09 12:13:39 +0100

    Added an additional test

--- test/e2e/product-display-scenarios.js
+++ test/e2e/product-display-scenarios.js
@@ -5,13 +5,17 @@ describe('Given we are displaying products', function() {
     browser.get('products.html');
 
     describe('When displaying a products detail', function() {
+        it('should have 3 products', function() {
+          var elems = element.all(by.repeater('product in products'));
+          expect(elems.count()).toBe(3);
+        });
 
         it('should load the home page', function() {
             var ptor = protractor.getInstance();
             var ele = by.id('tp');
             expect(ptor.isElementPresent(ele)).toBe(true);
         });
-        
+
         it('should be able to get product title', function() {
             var productTitle = element(by.repeater('product in products').row(0).column('{{product.name}}'));
             expect(productTitle).not.toBeNull();
